731c2c7d3168f4ae0767b4d622c985fd69456d6a
Tarek Ali <tarekali15@outlook.com>
2022-05-08 02:08:12 -0500

    internal api, QOL improvements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-import fetchDeps from "./utils/fetchdeps";
+import axios from "axios";
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-fetchDeps(
-  `https://raw.githubusercontent.com/${process.argv[2]}/master/package.json`
-);
+app.get("/repos", (req: any, response: any) => {
+  axios
+    .get(
+      `https://raw.githubusercontent.com/${req.query.org}/${req.query.repo}/master/package.json`
+    )
+    .then((res) => {
+      if (res.status !== 200) {
+        throw new Error(`Failed to fetch`);
+      }
+      const dependencies = [];
+      for (const dep in res.data.dependencies) {
+        dependencies.push({
+          dependency: dep,
+          version: res.data.dependencies[dep],
+        });
+      }
+      const devDependencies = [];
+      for (const depDev in res.data.devDependencies) {
+        devDependencies.push({
+          dependency: depDev,
+          version: res.data.devDependencies[depDev],
+        });
+      }
+      response.send(dependencies.concat(devDependencies));
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
