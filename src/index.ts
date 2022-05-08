87d8eb7680c8537e6528bbc79165e6e8099d4f60
Tarek Ali <tarekali15@outlook.com>
2022-05-08 10:18:01 -0500

    fallthrough options and real client-tooling

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,47 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/repos/:org/:repo", (req: any, response: any) => {
-  axios
-    .get(
-      `https://raw.githubusercontent.com/${req.params.org}/${req.params.repo}/master/package.json`
-    )
-    .then((res) => {
-      if (res.status !== 200) {
-        throw new Error(`Failed to fetch`);
-      }
-      const dependencies = [];
-      for (const dep in res.data.dependencies) {
-        dependencies.push({
-          dependency: dep,
-          version: res.data.dependencies[dep],
+  const option = req.query.option;
+  switch (option) {
+    case "packages":
+      axios
+        .get(
+          `https://raw.githubusercontent.com/${req.params.org}/${req.params.repo}/master/package.json`
+        )
+        .then((res) => {
+          if (res.status !== 200) {
+            throw new Error(`Failed to fetch`);
+          }
+          const dependencies = [];
+          for (const dep in res.data.dependencies) {
+            dependencies.push({
+              dependency: dep,
+              version: res.data.dependencies[dep],
+            });
+          }
+          const devDependencies = [];
+          for (const depDev in res.data.devDependencies) {
+            devDependencies.push({
+              dependency: depDev,
+              version: res.data.devDependencies[depDev],
+            });
+          }
+          response.send(dependencies.concat(devDependencies));
+        })
+        .catch((err) => {
+          response.status(500).send(err);
         });
-      }
-      const devDependencies = [];
-      for (const depDev in res.data.devDependencies) {
-        devDependencies.push({
-          dependency: depDev,
-          version: res.data.devDependencies[depDev],
-        });
-      }
-      response.send(dependencies.concat(devDependencies));
-    })
-    .catch((err) => {
-      response.status(500).send(err);
-    });
+      break;
+    case "webpack":
+      response.send("not available yet");
+      break;
+    case "rollup":
+      response.send("not available yet");
+      break;
+    default:
+      response.send("invalid option");
+      break;
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
